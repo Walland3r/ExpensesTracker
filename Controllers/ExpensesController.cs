8bc8fddfcbc1decd54b7d2ba015ca90d75084f72
Walland3r <oswalawender@gmail.com>
2024-12-19 19:25:20 +0100

    Cleaning nad status bars

--- Controllers/ExpensesController.cs
+++ Controllers/ExpensesController.cs
@@ -3,24 +3,21 @@ using ExpenseTracker.Models;
 using ExpenseTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
 
 public class ExpensesController : Controller
 {
     private readonly ExpenseTrackerContext _context;
-    private readonly ILogger<ExpensesController> _logger;
 
     public ExpensesController(ExpenseTrackerContext context, ILogger<ExpensesController> logger)
     {
         _context = context;
-        _logger = logger;
     }
 
     public async Task<IActionResult> Index()
     {
         var expenses = await _context.Expenses
             .Include(e => e.Category)
-            .Include(e => e.Budget) // Include Budget
+            .Include(e => e.Budget)
             .ToListAsync();
         var categories = await _context.Categories.ToListAsync();
         var budgets = await _context.Budgets.ToListAsync();
@@ -62,26 +59,8 @@ public class ExpensesController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> EditBudget(int id, Budget budget)
     {
-        if (id != budget.Id)
-        {
-            return NotFound();
-        }
-        try
-        {
-            _context.Update(budget);
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!BudgetExists(budget.Id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
+        _context.Update(budget);
+        await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
     }
 
@@ -89,17 +68,12 @@ public class ExpensesController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> AddExpenseToBudget(int budgetId, Expense expense)
     {
-        expense.BudgetId = budgetId; // Set the BudgetId
-        expense.CategoryId = expense.CategoryId; // Set the CategoryId
+        expense.BudgetId = budgetId;
+        expense.CategoryId = expense.CategoryId;
         _context.Expenses.Add(expense);
         await _context.SaveChangesAsync();
 
         return RedirectToAction(nameof(Index));
-    }
-
-    private bool BudgetExists(int id)
-    {
-        return _context.Budgets.Any(e => e.Id == id);
     }
 }
 
